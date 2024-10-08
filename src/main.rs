d7b7c10a81424ba60799b9f170dd65ba7cbcb30b
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2024-10-09 00:04:00 +0200

    Parse --tmux CLI arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::sync::atomic::AtomicU32;
 
 use gtk4::gdk::Display;
-use gtk4::{Align, Box, Button, CssProvider, Orientation, PackType, WindowControls, WindowHandle};
+use gtk4::gio::ApplicationFlags;
+use gtk4::{Align, Box as GtkBox, Button, CssProvider, Orientation, PackType, WindowControls, WindowHandle};
 use libadwaita::prelude::*;
 use libadwaita::{Application, ApplicationWindow, TabBar, TabView};
 
@@ -10,9 +11,9 @@ use toplevel::create_tab;
 
 mod global_state;
 mod keyboard;
+mod paned;
 mod terminal;
 mod toplevel;
-mod paned;
 
 static GLOBAL_TAB_ID: AtomicU32 = AtomicU32::new(0);
 
@@ -29,84 +30,105 @@ fn load_css() {
     );
 }
 
+fn build_application(app: &Application) {
+    // Create a new window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title(APPLICATION_TITLE)
+        .default_width(INITIAL_WIDTH)
+        .default_height(INITIAL_HEIGHT)
+        .build();
+
+    // Initialize global settings
+    // let global_settings = GlobalSettings::new(app);
+    println!("Build_application called");
+
+    // Window content box holds title bar and panes
+    let window_box = GtkBox::new(Orientation::Vertical, 0);
+
+    // View stack holds all panes
+    let tab_view = TabView::new();
+
+    // Close the tab_view when 0 tabs remain
+    let _window = window.clone();
+    tab_view.connect_close_page(move |tab_view, _page| {
+        if tab_view.n_pages() < 2 {
+            _window.close();
+        }
+        false
+    });
+
+    // Create initial Tab
+    create_tab(&tab_view);
+
+    // Terminal settings
+    let settings_button = Button::with_label("Settings");
+    let _app = app.clone();
+    settings_button.connect_clicked(move |_button| {
+        show_settings_window(_app.clone());
+    });
+
+    // View switcher for switching between open tabs
+    let tab_bar = TabBar::builder()
+        .css_classes(vec!["inline"])
+        .margin_top(0)
+        .margin_bottom(0)
+        .halign(Align::Fill)
+        .hexpand(true)
+        .autohide(false)
+        .can_focus(false)
+        .expand_tabs(false)
+        .view(&tab_view)
+        .end_action_widget(&settings_button)
+        .build();
+
+    // Header box holding tabs and window controls
+    let left_window_controls = WindowControls::new(PackType::Start);
+    let right_window_controls = WindowControls::new(PackType::End);
+    let header_box = GtkBox::new(Orientation::Horizontal, 0);
+    header_box.append(&left_window_controls);
+    header_box.append(&tab_bar);
+    header_box.append(&right_window_controls);
+
+    // Header bar
+    let window_handle = WindowHandle::builder()
+        .child(&header_box)
+        .css_classes(vec!["header-margin"])
+        .build();
+
+    window_box.append(&window_handle);
+    window_box.append(&tab_view);
+
+    window.set_content(Some(&window_box));
+    window.present();
+}
+
 fn main() -> glib::ExitCode {
     let application = Application::builder()
         .application_id("com.tomiyou.ivyTerm")
+        .flags(ApplicationFlags::HANDLES_COMMAND_LINE)
         .build();
 
-    application.connect_startup(|_| load_css());
-
-    application.connect_activate(move |app| {
-        // Create a new window
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title(APPLICATION_TITLE)
-            .default_width(INITIAL_WIDTH)
-            .default_height(INITIAL_HEIGHT)
-            .build();
-
-        // Initialize global settings
-        // let global_settings = GlobalSettings::new(app);
-
-        // Window content box holds title bar and panes
-        let window_box = Box::new(Orientation::Vertical, 0);
-
-        // View stack holds all panes
-        let tab_view = TabView::new();
-
-        // Close the tab_view when 0 tabs remain
-        let _window = window.clone();
-        tab_view.connect_close_page(move |tab_view, _page| {
-            if tab_view.n_pages() < 2 {
-                _window.close();
+    application.connect_command_line(move |app, cli| {
+        let args = cli.arguments();
+        let mut iterator = args.iter();
+        while let Some(arg) = iterator.next() {
+            if arg.eq_ignore_ascii_case("--tmux") {
+                // println!("Launching as tmux");
+                if let Some(ssh) = iterator.next() {
+                    println!("Attaching TMUX remote on {:?}", ssh);
+                } else {
+                    println!("Attaching TMUX locally");
+                }
             }
-            false
-        });
+        }
 
-        // Create initial Tab
-        create_tab(&tab_view);
-
-        // Terminal settings
-        let settings_button = Button::with_label("Settings");
-        let _app = app.clone();
-        settings_button.connect_clicked(move |_button| {
-            show_settings_window(_app.clone());
-        });
-
-        // View switcher for switching between open tabs
-        let tab_bar = TabBar::builder()
-            .css_classes(vec!["inline"])
-            .margin_top(0)
-            .margin_bottom(0)
-            .halign(Align::Fill)
-            .hexpand(true)
-            .autohide(false)
-            .can_focus(false)
-            .expand_tabs(false)
-            .view(&tab_view)
-            .end_action_widget(&settings_button)
-            .build();
-
-        // Header box holding tabs and window controls
-        let left_window_controls = WindowControls::new(PackType::Start);
-        let right_window_controls = WindowControls::new(PackType::End);
-        let header_box = Box::new(Orientation::Horizontal, 0);
-        header_box.append(&left_window_controls);
-        header_box.append(&tab_bar);
-        header_box.append(&right_window_controls);
-
-        // Header bar
-        let window_handle = WindowHandle::builder()
-            .child(&header_box)
-            .css_classes(vec!["header-margin"])
-            .build();
-
-        window_box.append(&window_handle);
-        window_box.append(&tab_view);
-
-        window.set_content(Some(&window_box));
-        window.present();
+        // Handle this better
+        app.activate();
+        0
     });
 
+    application.connect_startup(|_| load_css());
+    application.connect_activate(build_application);
     application.run()
 }
