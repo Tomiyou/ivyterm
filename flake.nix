98b4779f15951786e4a8b45aca96a9e46e21c20a
Jomar Milan <jomarm@jomarm.com>
2025-01-03 14:53:00 -0800

    Add dev shell with toolchain

--- flake.nix
+++ flake.nix
@@ -2,12 +2,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
         # cargo-nix = import ./Cargo.nix { inherit pkgs; };
         cargo-nix = import ./Cargo.nix {
           inherit pkgs;
@@ -23,10 +30,14 @@
           };
         };
         ivyterm = cargo-nix.rootCrate.build;
+        toolchain = pkgs.rust-bin.stable.latest.default;
       in {
         packages = {
           inherit ivyterm;
           default = ivyterm;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ toolchain ];
         };
       }
     );
