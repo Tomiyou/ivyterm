ea38bbbe1b283910ee1a3a6997eddb8154e755f5
Jomar Milan <jomarm@jomarm.com>
2025-01-03 16:57:20 -0800

    Use fenix for toolchain, add devshell

--- flake.nix
+++ flake.nix
@@ -6,12 +6,20 @@
       url = "github:nix-community/crate2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, crate2nix }:
+  outputs = { self, nixpkgs, flake-utils, crate2nix, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+        toolchain = fenix.packages.${system}.stable.defaultToolchain;
         cargo-nix = pkgs.callPackage (crate2nix.tools.${system}.generatedCargoNix {
           name = "ivyterm";
           src = ./.;
@@ -33,6 +41,9 @@
         packages = {
           inherit ivyterm;
           default = ivyterm;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ toolchain pkgs.libadwaita pkgs.vte-gtk4 pkgs.openssl pkgs.pkg-config ];
         };
       }
     );
